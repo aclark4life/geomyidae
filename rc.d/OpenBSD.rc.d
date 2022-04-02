4ea20bd4ac233b244a34ee294b99e37df75a4d41
Christoph Lohmann <20h@r-36.net>
2022-04-02 22:55:58 +0200

    Move geomyidae on OpenBSD to /usr/local/bin.

--- rc.d/OpenBSD.rc.d
+++ rc.d/OpenBSD.rc.d
@@ -2,7 +2,7 @@
 #
 # $OpenBSD: geomyidae,v 1.7 2017/06/30 22:06:09 anonymous Exp $
 
-daemon="/usr/local/sbin/geomyidae"
+daemon="/usr/local/bin/geomyidae"
 daemon_flags="-l /var/log/geomyidae.log -u _geomyidae -g _geomyidae"
 
 . /etc/rc.d/rc.subr
