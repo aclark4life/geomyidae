3a876cbe4fb9b93ecf98decfa79e7f9b27dbc320
Christoph Lohmann <20h@r-36.net>
2023-07-22 17:14:59 +0200

    First stub for new http-compatibility feature.

--- main.c
+++ main.c
@@ -187,6 +187,25 @@ handlerequest(int sock, char *req, int rlen, char *base, char *ohost,
 	if (c)
 		c[0] = '\0';
 
+	/*
+	 * Try to guess if we have some HTTP-like protocol compatibility
+	 * mode.
+	 */
+	if (!nocgi && recvb[0] != '/' && (c = strchr(recvb, " "))) {
+		*c = '\0';
+		if (strchr(recvb, '/'))
+			goto dothegopher;
+		if (snprintf(path, "%s/%s", base, recvb) <= sizeof(path)) {
+			if (realpath(path, (char *)rpath)) {
+				if (stat(rpath, &dir)) {
+					handlecgi(sock, rpath, port, base, NULL, NULL, ohost,
+						clienth, serverh, istls, req, NULL);
+				}
+			}
+		}
+	}
+dothegopher:
+
 	/* Do not allow requests including "..". */
 	if (strstr(recvb, "..")) {
 		dprintf(sock, "%s", selinval);
